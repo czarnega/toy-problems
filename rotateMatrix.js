456ff264e8946eccf764334037d9b8d69320be7e
Colin Zarnegar <c.zarnegar@gmail.com>
2016-07-07 19:35:37 -0700

    adds solution to rotateMatrix toy problem

---
+++ rotateMatrix.js
@@ -0,0 +1,16 @@
+// Write a function that rotates a NxN matrix 90 degrees clockwise.
+// let array = [[1, 2, 3, 4],[5, 6, 7, 8],[9, “A”,”B”,”C”],[“D”,”E”,”F”,”G”]];
+// rotateMatrix(array); => [[“D”, 9, 5, 1],[“E”, “A”, 6, 2],[“F”,”B”, 7, 3],[“G”,”C”, 8, 4]]
+const rotateMatrix = (matrix) => {
+  var result = [];
+  // work from bottom up, and push each value of array into corresponding row
+  for(var i = matrix.length - 1; i >= 0; i--){
+    for(var j = 0; j <= matrix.length - 1; j++){
+  	  if(i === matrix.length - 1){
+  	  	result.push([]);
+  	  }
+      result[j].push(matrix[i][j]);
+    }
+  }
+  return result;
+}
