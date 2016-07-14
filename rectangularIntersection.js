b48e4ebe045fcef6571b02c4316e7241fb236e00
Colin Zarnegar <c.zarnegar@gmail.com>
2016-07-13 18:15:42 -0700

    fixes solution to rectangularIntersection

--- rectangularIntersection.js
+++ rectangularIntersection.js
@@ -16,7 +16,7 @@ function rectangularIntersection(rectangleA, rectangleB){
 		if(A.leftX < B.leftX){
 			if(A.leftX + A.width > B.leftX){
 				intersection.leftX = B.leftX;
-				intersection.width = A.leftX + A.width - B.leftX;
+				intersection.width = Math.min((A.leftX + A.width - B.leftX),B.width);
 			} else {
 				return null;
 			}
@@ -25,7 +25,7 @@ function rectangularIntersection(rectangleA, rectangleB){
 		else if(A.leftX > B.leftX){
 			if(B.leftX + B.width > A.leftX){
 				intersection.leftX = A.leftX;
-				intersection.width = B.leftX + B.width - A.leftX;
+				intersection.width = Math.min((B.leftX + B.width - A.leftX),A.width);
 			} else {
 				return null;
 			}
@@ -42,7 +42,7 @@ function rectangularIntersection(rectangleA, rectangleB){
 		if(A.bottomY < B.bottomY){
 			if(A.bottomY + A.height > B.bottomY){
 				intersection.bottomY = B.bottomY;
-				intersection.height = A.bottomY + A.height - B.bottomY;
+				intersection.height = Math.min((A.bottomY + A.height - B.bottomY),B.height);
 			} else {
 				return null;
 			}
@@ -51,7 +51,7 @@ function rectangularIntersection(rectangleA, rectangleB){
 		else if(A.bottomY > B.bottomY){
 			if(B.bottomY + B.height > A.bottomY){
 				intersection.bottomY = A.bottomY;
-				intersection.height = B.bottomY + B.height - A.bottomY;
+				intersection.height = Math.min((B.bottomY + B.height - A.bottomY),A.height);
 			} else {
 				return null;
 			}
@@ -64,28 +64,3 @@ function rectangularIntersection(rectangleA, rectangleB){
 	}
 	return Object.keys(intersection).length === 4 ? intersection : null;
 }
-
-
-// // case if rectangleA is further left of rectangleB
-// if(rectangleA.leftX < rectangleB.leftX){
-// 	if(rectangleA.leftX + rectangleA.width > rectangleB.leftX){
-// 		intersection.leftX = rectangleB.leftX;
-// 		intersection.width = rectangleA.leftX + rectangleA.width - rectangleB.leftX;
-// 	} else {
-// 		return null;
-// 	}
-// }
-// // case if rectangleB is further left of rectangleA
-// else if(rectangleA.leftX > rectangleB.leftX){
-// 	if(rectangleB.leftX + rectangleB.width > rectangleA.leftX){
-// 		intersection.leftX = rectangleA.leftX;
-// 		intersection.width = rectangleB.leftX + rectangleB.width - rectangleA.leftX;
-// 	} else {
-// 		return null;
-// 	}
-// }
-// // case if rectangles have same starting leftX
-// else {
-// 	intersection.leftX = rectangleA.leftX;
-// 	intersection.width = Math.min(rectangleA.width,rectangleB.width);
-// }
