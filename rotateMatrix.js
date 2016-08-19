df680ca401e86700ac5272a9b930052961a58d29
Colin Zarnegar <c.zarnegar@gmail.com>
2016-08-19 14:40:17 -0700

    adds additional solution to rotateMatrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -14,3 +14,17 @@ const rotateMatrix = (matrix) => {
   }
   return result;
 }
+
+// function rotateMatrix (matrix) {
+//   var rotated = [];
+//   for (var i = 0; i < matrix.length; i++) {
+//     var array = [];
+//     for (var j = 0; j < matrix[i].length; j++) {
+//       // adds to the beginning of the array
+//       // and switches the order of matrix
+//       array.unshift(matrix[j][i]);
+//     }
+//     rotated.push(array);
+//   }
+//   return rotated;
+// }
