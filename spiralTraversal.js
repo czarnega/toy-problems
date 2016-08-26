1e7fd7e4a3c9d9cd1bc3897df2a33bf1b7fde06e
Colin Zarnegar <c.zarnegar@gmail.com>
2016-08-26 13:52:22 -0700

    adds sample input/output to spiralTraversal toy problem

--- spiralTraversal.js
+++ spiralTraversal.js
@@ -1,5 +1,8 @@
 // Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
 // and prints out every value found, but in a spiral from the upper left in to the center.
+// Example:
+// input = [ [ 1, 2, 3, 4, 5 ], [ 6, 7, 8, 9, 10 ], [ 11, 12, 13, 14, 15 ], [ 16, 17, 18, 19, 20 ], [ 21, 22, 23, 24, 25 ] ]
+// output = [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
 
 function spiralTraversal (matrix) {
 
