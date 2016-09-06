efe065076a02c8e8b3a5bd4fb188d4526cfb8e84
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-05 18:17:42 -0700

    adds initial solution to timeInWords toy problem

--- timeInWords.js
+++ timeInWords.js
@@ -11,15 +11,82 @@ function timeBuilder(hour,minutes){
 
 	if (minutes < 30) {
 		hourString = hourToString(hour);
+		if(minutes === 1 || minutes === 59){
+			minuteString += ' minute ';
+		} else if (minutes === 15 || minutes === 45){
+			minuteString += ' ';
+		} else if (minutes === 0){
+			return hourString + ' o\' clock';
+		} else {
+			minuteString += ' minutes ';
+		}
+		return minuteString + 'past ' + hourString;
 	} else if (minutes === 30){
 		hourString = hourToString(hour);
+		return 'half past ' + hourString;
 	} else {
 		hourString = hourToString(hour+1);
+		minuteString = minutesToString(60 - minutes);
+		if(minutes === 1 || minutes === 59){
+			minuteString += ' minute ';
+		} else if (minutes === 15 || minutes === 45){
+			minuteString += ' ';
+		} else {
+			minuteString += ' minutes ';
+		}
+		return minuteString + 'to ' + hourString;
 	}
 }
 
 function minutesToString(minutes){
-
+	if(minutes <= 20){
+		switch(minutes){
+			case 1:
+				return 'one';
+			case 2:
+				return 'two';
+			case 3:
+				return 'three';
+			case 4:
+				return 'four';
+			case 5:
+				return 'five';
+			case 6:
+				return 'six';
+			case 7:
+				return 'seven';
+			case 8:
+				return 'eight';
+			case 9:
+				return 'nine';
+			case 10:
+				return 'ten';
+			case 11:
+				return 'eleven';
+			case 12:
+				return 'twelve';
+			case 13:
+				return 'thirteen';
+			case 14:
+				return 'fourteen';
+			case 15:
+				return 'quarter';
+			case 16:
+				return 'sixteen';
+			case 17:
+				return 'seventeen';
+			case 18:
+				return 'eighteen';
+			case 19:
+				return 'nineteen';
+			case 20:
+				return 'twenty';
+			default:
+				return 'Invalid minutes';
+		}
+	} else {
+		return 'twenty ' + minutesToString(minutes - 20);
+	}
 }
 
 function hourToString(hour){
