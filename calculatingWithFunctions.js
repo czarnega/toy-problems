cdf62019d0128023b181326a60982a59e1e94e4f
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-06 17:04:41 -0700

    adds calculatingWithFunctions toy problem to repo

---
+++ calculatingWithFunctions.js
@@ -0,0 +1,26 @@
+// Writes functions that calculate operations such as the following:
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+// Requirements:
+// - There must be a function for each number from 0 ("zero") to 9 ("nine")
+// - There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+// - Each calculation consist of exactly one operation and two numbers
+// - The most outer function represents the left operand, the most inner function represents the right operand
+
+function zero(operator) {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
