e42b30f87a70f99ca8c8e934d834b98715a8fd82
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-06 17:16:43 -0700

    adds initial solution to calculatingWithFunctions toy problem

--- calculatingWithFunctions.js
+++ calculatingWithFunctions.js
@@ -9,18 +9,84 @@
 // - Each calculation consist of exactly one operation and two numbers
 // - The most outer function represents the left operand, the most inner function represents the right operand
 
-function zero(operator) {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function zero(operation) {
+	if(arguments.length === 0){
+		return 0;
+	}
+	return operation(0);
+}
+function one(operation) {
+	if(arguments.length === 0){
+		return 1;
+	}
+	return operation(1);
+}
+function two(operation) {
+	if(arguments.length === 0){
+		return 2;
+	}
+	return operation(2);
+}
+function three(operation) {
+	if(arguments.length === 0){
+		return 3;
+	}
+	return operation(3);
+}
+function four(operation) {
+	if(arguments.length === 0){
+		return 4;
+	}
+	return operation(4);
+}
+function five(operation) {
+	if(arguments.length === 0){
+		return 5;
+	}
+	return operation(5);
+}
+function six(operation) {
+	if(arguments.length === 0){
+		return 6;
+	}
+	return operation(6);
+}
+function seven(operation) {
+	if(arguments.length === 0){
+		return 7;
+	}
+	return operation(7);
+}
+function eight(operation) {
+	if(arguments.length === 0){
+		return 8;
+	}
+	return operation(8);
+}
+function nine(operation) {
+	if(arguments.length === 0){
+		return 9;
+	}
+	return operation(9);
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function plus(innerNum){
+	return function(outerNum){
+		return outerNum + innerNum
+	}
+}
+function minus(innerNum){
+	return function(outerNum){
+		return outerNum - innerNum
+	}
+}
+function times(innerNum){
+	return function(outerNum){
+		return outerNum * innerNum
+	}
+}
+function dividedBy(innerNum){
+	return function(outerNum){
+		return outerNum / innerNum
+	}
+}
