7e25447e58ab4aa7279c12fb12fb928429da15ac
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-15 14:38:47 -0400

    adds climbingStairs toy problem to repo

---
+++ climbingStairs.js
@@ -0,0 +1,12 @@
+// You are climbing a stair case. It takes n steps to reach to the top. 
+// Your solution will need to be optimized to run in O(n) time complexity.
+// 
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Sample Input 0: 5
+// Sample Output 0: 8
+// Sample Input 1: 10
+// Sample Output 1: 89
+
+function climbStairs (n) {
+  
+}
