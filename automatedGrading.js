c7be553d69d6df21539a5f0ced675ca184f57462
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-14 22:00:08 -0400

    adds initial solution for automatedGrading toy problem

--- automatedGrading.js
+++ automatedGrading.js
@@ -8,5 +8,16 @@
 // { "all": 15.93, "Jon": 15.8, "Jeremy": 13.4, "Jesse": 18.6 }
 
 function autograde (report) {
-  
+	let runningAverage = 0, results = {};
+  let students = report.split('\n');
+  students.forEach(studentLine => {
+  	let studentData = studentLine.split(' ');
+  	let name = studentData.splice(0,1)[0];
+  	let gradeSum = studentData.map(Number).reduce(((sum,grade) => sum + grade),0)
+  	let average = ( gradeSum / studentData.length).toFixed(2);
+  	results[name] = parseFloat(average);
+  	runningAverage += parseFloat(average);
+  })
+  results.all = parseFloat((runningAverage/students.length).toFixed(2));
+  return results
 }
