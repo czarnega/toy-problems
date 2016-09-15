a8c41e029cce0b962672fa9ebd44e44b50928816
Colin Zarnegar <c.zarnegar@gmail.com>
2016-09-14 21:43:45 -0400

    adds automatedGrading toy problem to repo

---
+++ automatedGrading.js
@@ -0,0 +1,12 @@
+// Given a report of all student grades, return both
+// the class average and the average of each student.
+// NOTE: Round any calculations to the nearest 2 decimal places.
+
+// Sample Input:
+// "Jon 19 14 15 15 16\nJeremy 15 11 10 15 16\nJesse 19 17 20 19 18"
+// Sample Output:
+// { "all": 15.93, "Jon": 15.8, "Jeremy": 13.4, "Jesse": 18.6 }
+
+function autograde (report) {
+  
+}
